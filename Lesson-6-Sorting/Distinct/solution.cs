63489ba3e77f6c0146fb2d485977389f698bb481
Jon Savage <jon5@vt.edu>
2017-02-05 12:29:56 -0500

    Lesson 6 Distinct

---
+++ Lesson-6-Sorting/Distinct/solution.cs
@@ -0,0 +1,14 @@
+using System;
+// you can also use other imports, for example:
+// using System.Collections.Generic;
+using System.Linq;
+
+// you can write to stdout for debugging purposes, e.g.
+// Console.WriteLine("this is a debug message");
+
+class Solution {
+    public int solution(int[] A) {
+        // write your code in C# 6.0 with .NET 4.5 (Mono)
+    return A.ToList().Distinct().Count();
+    }
+}
